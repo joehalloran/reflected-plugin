99b820f8a265142ddbfa1df0ea8012bb0af3d02a
joehalloran <joestandard@Mac-Pro-Joe.local>
2016-06-30 16:33:16 +0100

    css and front page

--- reflected/reflected.php
+++ reflected/reflected.php
@@ -9,19 +9,19 @@
  * that starts the plugin.
  *
  * @link              http://example.com
- * @since             1.0.0
- * @package           Plugin_Name
+ * @since             1.1.0
+ * @package           reflected
  *
  * @wordpress-plugin
  * Plugin Name:       ReflectED
  * Plugin URI:        http://example.com/plugin-name-uri/
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.0.0
- * Author:            Your Name or Your Company
+ * Description:       Custom plugin for the ReflectED website to add custom post types (lessons) and taxonomy. Also adds custom fields (to upload lesson plans)
+ * Version:           1.1.0
+ * Author:            Joe Halloran
  * Author URI:        http://example.com/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       plugin-name
+ * Text Domain:       reflected
  * Domain Path:       /languages
  */
 
--- reflected_custom_theme/front-page.php
+++ reflected_custom_theme/front-page.php
@@ -12,7 +12,6 @@
         <?php // theloop
         if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-            <h2 class="page-header"><?php the_title() ;?></h2>
             <?php the_content(); ?>
             <?php wp_link_pages(); ?>
             <?php comments_template(); ?>
